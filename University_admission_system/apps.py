19d83af8487274cae818541b213899682c6f488d
Nikhil Telang <nikhiltelang34@gmail.com>
2021-07-26 18:12:12 +0530

    second commit

---
+++ University_admission_system/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UniversityAdmissionSystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'University_admission_system'
