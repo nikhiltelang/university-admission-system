97a52a8208689a1a6fd47a5c5bdbb950f11d3a4e
Nikhil Telang <nikhiltelang34@gmail.com>
2021-07-27 12:01:24 +0530

    login and sign work is done

--- University_admission_system/urls.py
+++ University_admission_system/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     url(r'^$', views.home,name='home'),
     # path('contact',views.contact,name='contact')
-
+    path('signin',views.signin,name='signin'),
+    path('signup',views.signup,name='signup'),
 ]
--- University_admission_system/views.py
+++ University_admission_system/views.py
@@ -1,10 +1,47 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 def home(request):
-    print("jump here")
     return render(request,'index.html')
 
 # def contact(request):
 #     print("jump here")
 #     return
 
+def signin(request):
+    if request.method == 'POST':
+        signintype = request.POST['signintypeselect']
+        email = request.POST['SigninInputEmail1']
+        passw = request.POST['SigninInputPassword1']
+        # user = authenticate(username=usern, password=passw)
+        print(signintype,email, passw)
+        # if user is not None:
+        #     login(request, user)
+        #     print("success")
+        #     messages.success(request, 'Successfully Logged In')
+        #     return redirect('home')
+        #
+        # else:
+        #     messages.error(request, 'Invalid Creditions')
+        #     print("fail")
+        #     return redirect('home')
+        return HttpResponse("Login Successfully")
+
+
+def signup(request):
+    if request.method=='POST':
+        signuptype = request.POST['signuptypeselect']
+        fname = request.POST.get('SignupInputFullName')
+        email = request.POST.get('SignupInputEmail1')
+        pass1 = request.POST.get('SignupInputPassword1')
+        pass2 = request.POST.get('SignupInputPassword2')
+        # if pass1 != pass2:
+        #     messages.error(request,'Password must be same')
+        # myuser = User.objects.create_user(username=username,email=email,password=pass1)
+        # myuser.first_name = fname
+        # myuser.last_name = lname
+        # myuser.save()
+        # messages.success(request,'Your account have been Created')
+        print(signuptype,fname,email,pass1,pass2)
+        return HttpResponse("Signup Successfully")
